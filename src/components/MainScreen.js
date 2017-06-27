c8d5eb683300345b1c26ea86f2223c7f76eef0d9
Marwil96 <william_martinsson@hotmail.com>
2017-06-27 13:16:04 +0200

    Added GoogleMaps Functionality

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -2,48 +2,50 @@
 
 import React, { Component } from 'react';
 import { Text, TextInput } from 'react-native';
+import MapView from 'react-native-maps';
 import { CardSection, Card } from './common';
 
 class Main extends Component {
 
-	constructor(props) {
- super(props);
- this.state = { text: 'PlaceHolder' };
-  }
 
 	render() {
 		const { textStyle, inputStyle } = styles;
 		
 		return (
-			<CardSection>
-				<Card>
-					<Text style={textStyle}> Wop </Text>
-				</Card>
-				<TextInput 
-				{...this.props}
-				style={inputStyle}
-				multiline
-				value={this.state.text}
-				onChangeText={(text) => this.setState({ text })}
-				editable
-				/>
-			</CardSection>
+			
+				<MapView
+					style={textStyle}
+				    initialRegion={{
+				      latitude: 37.78825,
+				      longitude: -122.4324,
+				      latitudeDelta: 0.0922,
+				      longitudeDelta: 0.0421,
+				    }}
+				  />
+				
+		
 		);
 	}
 }
 
 const styles = {
 	textStyle: {
-		fontSize: 18
+		position: 'absolute',
+	    top: 50,
+	    left: 15,
+	    right: 0,
+	    bottom: 0,
+	    width: 325,
+	    height: 400
 	},
 	inputStyle: {
-		fontSize: 18,
-		height: 300,
-		width: 300,
-		borderColor: 'black',
-		borderWidth: 5,
-		padding: 26,
-		marginTop: 300
+		   position: 'absolute',
+	    top: 0,
+	    left: 0,
+	    right: 0,
+	    bottom: 0,
+	    justifyContent: 'flex-end',
+	    alignItems: 'center',
 	}
 };
 
